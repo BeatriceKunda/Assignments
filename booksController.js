76b498543ec6083d0ffa222d0e3344fc62dc89af
BeatriceKunda <beatrice.kunda96@gmail.com>
2020-04-09 21:09:13 +0300

    displays all books published by O'Reilly Media

--- booksController.js
+++ booksController.js
@@ -1,5 +1,8 @@
 const books = require('./books');
 
+
+// Display all books and their authors 
+
 const booksLength = books.length;
 console.log("Here is a list of all books and their authors")
 for (let i = 0; i < booksLength; i++) {
@@ -7,4 +10,15 @@ for (let i = 0; i < booksLength; i++) {
 
     console.log(`Book: ${books[i].title}`)
     console.log(`Author: ${books[i].author}\n`)
+}
+
+// Display all the books (show only book titles) published by `O'Reilly Media`.
+
+console.log("\nThese were the books published by O'Reilly Media")
+console.log(`....................................................................................\n`);
+
+for (let i = 0; i < booksLength; i++) {
+    if (books[i].publisher === "O'Reilly Media") {
+        console.log(books[i].title);
+    }
 }
